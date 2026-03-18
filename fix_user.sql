3bc60eaf6d4c289aa44c77a8a8990fd8f436beae
bangsmackpow <username@users.noreply.github.com>
2026-03-18 09:03:20 -0500

    feat: Add authentication middleware and update a user's password hash.

--- fix_user.sql
+++ fix_user.sql
@@ -1,1 +1,1 @@
-UPDATE users SET password_hash = '$2a$10$Sh10tCrL8D1hC4mYYQh.6O1swSHZJHll3bHK205EHhoLPjsdPw1mz6' WHERE email = 'curtis@printfrenzy.dev';
+UPDATE users SET password_hash = '$2a$10$CBJzP4wCG1AT96xZkcTU5O7UxGKFT3ZfpqhhjzmfDUlVXZeUGZyLy' WHERE email = 'curtis@printfrenzy.dev';
